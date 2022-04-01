b964c6daa21a87cdc71283d5ca4c12f2043cd0de
Miran Germovšek <miran.germovsek10@gmail.com>
2022-04-01 16:39:42 +0200

    Declaring string variables

---
+++ ex01/src/index.js
@@ -0,0 +1,15 @@
+function main(){
+    
+    var myFirstName = "Miran";
+    var myLastName = "Germovšek";
+    var myHomeTotwn = "Bizeljsko";
+
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTotwn
+    };
+}
+
+console.log(main());
+module.exports = main;
